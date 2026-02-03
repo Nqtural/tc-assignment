cacc219cb2bd35fce6604e33f1f7f2a35a8d0f5a
Nqtural <robin.stalstad@proton.me>
2026-02-03 14:23:18 +0100

    refactor(backend): move logout logic to data layer

---
+++ backend/src/data/logout_user.rs
@@ -0,0 +1,32 @@
+use anyhow::{anyhow, Result};
+use deadpool_sqlite::rusqlite::params;
+
+use super::Database;
+
+pub enum LogoutOutcome {
+    Success,
+    NotLoggedIn,
+    InternalServerError,
+}
+
+impl Database {
+    pub async fn logout_user(&self, session_uuid: String) -> Result<LogoutOutcome> {
+        let conn = self.pool.get().await?;
+
+        let deleted = conn
+            .interact(move |conn| {
+                conn.execute(
+                    "DELETE FROM sessions WHERE uuid = ?1",
+                    params![session_uuid],
+                )
+            })
+            .await
+            .map_err(|e| anyhow!("{e}"))??;
+
+        if deleted > 0 {
+            Ok(LogoutOutcome::Success)
+        } else {
+            Ok(LogoutOutcome::NotLoggedIn)
+        }
+    }
+}
--- backend/src/data/mod.rs
+++ backend/src/data/mod.rs
@@ -1,6 +1,9 @@
 mod login_user;
 pub use login_user::LoginOutcome;
 
+mod logout_user;
+pub use logout_user::LogoutOutcome;
+
 mod register_user;
 pub use register_user::RegisterOutcome;
 
