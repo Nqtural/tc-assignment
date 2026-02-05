7b223f96eec18ea0431591d2ed3ccdbe88c74330
Nqtural <robin.stalstad@proton.me>
2026-02-05 21:56:03 +0100

    chore(backend): add `--dev` again
    Was accidentally removed when adding development CORS layer.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,11 +5,29 @@ use axum::{
     Router,
 };
 use tower_cookies::CookieManagerLayer;
+use std::env;
+
 use backend::{data::Database, routes};
 use backend::cors::dev_cors;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    
+    let mut dev = false;
+
+    for arg in env::args().skip(1) {
+        match arg.as_str() {
+            "--dev" => dev = true,
+            _ => {
+                println!(" ");
+            }
+        }
+    }
+
+    if dev {
+        println!("dev mode")
+    }
+
     let database = Database::new().await?;
 
     let app = Router::new()
@@ -22,7 +40,6 @@ async fn main() -> Result<()> {
         .route("/rooms/get", get(routes::rooms::get))
         .with_state(database)
         .layer(CookieManagerLayer::new())
-        // always dev mode for now
         .layer(dev_cors());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
